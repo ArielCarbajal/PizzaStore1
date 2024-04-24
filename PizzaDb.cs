87dc7e1f31c4dfab471fd343a529943009d7d212
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-04-23 21:53:06 -0600

    Agregar archivos de proyecto.

---
+++ PizzaDb.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaStore1;
+
+class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+}
